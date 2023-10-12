3be664da026a9fa4f1f65c3129a80d5bb85c32b8
luisalfred15 <luis.maestres@correo.unimet.edu.ve>
2023-10-11 23:14:08 -0400

    Avances en objeto Simulation

---
+++ src/classes/InitialValues.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+
+/**
+ *
+ * @author luisa
+ */
+public class InitialValues {
+    
+    private int lastDigit;
+    private int dayDuration;
+    private int deadline;
+    
+    public InitialValues(int lastDigit, int dayDuration, int deadline) {
+        this.lastDigit = lastDigit;
+        this.dayDuration = dayDuration;
+        this.deadline = deadline;
+    }
+
+    /**
+     * @return the lastDigit
+     */
+    public int getLastDigit() {
+        return lastDigit;
+    }
+
+    /**
+     * @param lastDigit the lastDigit to set
+     */
+    public void setLastDigit(int lastDigit) {
+        this.lastDigit = lastDigit;
+    }
+
+    /**
+     * @return the dayDuration
+     */
+    public int getDayDuration() {
+        return dayDuration;
+    }
+
+    /**
+     * @param dayDuration the dayDuration to set
+     */
+    public void setDayDuration(int dayDuration) {
+        this.dayDuration = dayDuration;
+    }
+
+    /**
+     * @return the deadline
+     */
+    public int getDeadline() {
+        return deadline;
+    }
+
+    /**
+     * @param deadline the deadline to set
+     */
+    public void setDeadline(int deadline) {
+        this.deadline = deadline;
+    }
+    
+    
+}
--- src/classes/Simulation.java
+++ src/classes/Simulation.java
@@ -4,6 +4,8 @@
  */
 package classes;
 
+import Developer.Developer;
+
 import java.util.concurrent.Semaphore;
 
 /**
@@ -13,7 +15,6 @@ import java.util.concurrent.Semaphore;
 public class Simulation {
     
     private int lastDigit;
-    private int[] parameters;
     int dayDuration;
     int deadline;
     int quantityScriptsDevs;
@@ -23,19 +24,27 @@ public class Simulation {
     int quantityDlcsDevs;
     int quantityIntegrators;
     Developer[] developers = new Developer[6];
+    int [] scriptsDevs = new int[quantityScriptsDevs];
     
+    public Simulation(InitialValues values, int[] devsQuantity) {
     // devsQuantity es el arreglo donde la posicion i-esima indica la cantidad de devs del tipo i
-    public Simulation(int lastDigit, int[] initialValues, int[] devsQuantity) {
-        this.lastDigit = lastDigit;
-        this.dayDuration = initialValues[0];
-        this.deadline = initialValues[1];
+    /*
+    0: guionistas
+    1: diseniador de niveles
+    2: diseniador de sprites
+    3: diseniador de sistemas
+    4: creador de DLCs
+    5: integradores
+    */
+        this.lastDigit = values.getLastDigit();
+        this.dayDuration = values.getDayDuration();
+        this.deadline = values.getDeadline();
         this.quantityScriptsDevs = devsQuantity[0];
         this.quantityLevelsDevs = devsQuantity[1];
         this.quantitySpritesDevs = devsQuantity[2];
         this.quantitySystemsDevs = devsQuantity[3];
         this.quantityDlcsDevs = devsQuantity[4];
         this.quantityIntegrators = devsQuantity[5];
-        this.developers = developers;
     }
     
     public void spawnDevelopers(int[] devsQuantity) {
@@ -57,18 +66,4 @@ public class Simulation {
         this.lastDigit = lastDigit;
     }
 
-    /**
-     * @return the parameters
-     */
-    public int[] getParameters() {
-        return parameters;
-    }
-
-    /**
-     * @param parameters the parameters to set
-     */
-    public void setParameters(int[] parameters) {
-        this.parameters = parameters;
-    }
-    
 }
